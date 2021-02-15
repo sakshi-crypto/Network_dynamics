fcba3753e869615b0a65ea1808415148f4716c08
sakshi-crypto <78174770+sakshi-crypto@users.noreply.github.com>
2021-02-15 19:49:13 +0100

    Update violin_community.R
    R code Community Size Visualization

--- violin_community.R
+++ violin_community.R
@@ -7,7 +7,7 @@ library(grid)
 library(gridExtra)
 library(data.table)
 violin_data<-list()
-setwd("/Users/sakshidhama/Dropbox/work/community_preference/data_setgraph")
+setwd("/Network_dynamics/datasetgraph")#path to excel files
 mydir<-"violinfiles"
 community_files<-list.files(path=mydir,pattern = "*.xlsx",full.names = TRUE)
 file_table<-lapply(community_files, read_excel)
@@ -24,7 +24,6 @@ for (i in 1:14) {
   zvalues[[i]]<-paste0(xvalues[[i]])
   col[[i]]<-rep(i,1,14)
 }
-
 graphs_dataframe<-data.table(x=unlist(xvalues),y=unlist(yvalues),z=unlist(zvalues))
 png(file = "violin2.png",width = 1600 ,height = 500 ,res =120)
 mar.default <- c(5,4,4,2) + 0.1
@@ -37,13 +36,13 @@ par(mar = mar.default + c(0, 4, 0, 0))
  #                                                                                                                                   axis.title.y = element_text(color = "grey20", size = 28, angle = 90, hjust = .5, vjust = .5, face = "plain"),
  #                                                                                                                                   legend.title = element_text(color = "black", size = 22),
  #                                                                                                                                 legend.text = element_text(color = "black", size = 20))
-
 newdp2 <- ggplot(graphs_dataframe,aes(x=graphs_dataframe$z,y=graphs_dataframe$y))+ scale_y_log10() +xlab(expression(paste(nu))) + ylab("Community size") + labs(fill=expression(paste(nu," values")))+
 geom_violin(aes(),draw_quantiles = TRUE) +geom_jitter(alpha=0.5, aes(color=graphs_dataframe$z),position = position_jitter(width = .2))+
-  theme(axis.text.x = element_text(color = "grey20", size = 18, angle = 90, hjust = .5, vjust = .5, face = "plain"),axis.text.y = element_text(color = "grey20", size = 18, angle = 0, hjust = 1, vjust = 0, face = "plain"),  
-                                                                                                                                                                                                                             axis.title.x = element_text(color = "grey20", size = 28, angle = 0, hjust = .5, vjust = 0, face = "plain"),
-                                                                                                                                                                                                                             axis.title.y = element_text(color = "grey20", size = 28, angle = 90, hjust = .5, vjust = .5, face = "plain"),
-                                                                                                                                                                                                                             legend.title = element_text(color = "black", size = 20),
-                                                                                                                                                                                                                             legend.text = element_text(color = "black", size = 18))
+  theme(axis.text.x = element_text(color = "grey20", size = 18, angle = 90, hjust = .5, vjust = .5, face = "plain"),
+  axis.text.y = element_text(color = "grey20", size = 18, angle = 0, hjust = 1, vjust = 0, face = "plain"),  
+  axis.title.x = element_text(color = "grey20", size = 28, angle = 0, hjust = .5, vjust = 0, face = "plain"),
+  axis.title.y = element_text(color = "grey20", size = 28, angle = 90, hjust = .5, vjust = .5, face = "plain"),
+  legend.title = element_text(color = "black", size = 20),
+  legend.text = element_text(color = "black", size = 18))
 grid.arrange(newdp2)
 dev.off()
